5f07aa795e84b79d493d1abbfee37633b8cb4b49
Dysoch <Dysoch@users.noreply.github.com>
2024-06-02 10:56:19 +0200

    more info

---
+++ jss/main.js
@@ -0,0 +1,13 @@
+﻿
+var gameloop = setInterval(function() {
+    if (ticking) return;
+    ticking = true;
+    updateUI();
+
+    // fps for debug only
+    //var thisFrameTime = (thisLoop = new Date) - lastLoop;
+    //frameTime += (thisFrameTime - frameTime) / filterStrength;
+    //lastLoop = thisLoop;
+
+    ticking = false;
+}, 1000)
--- jss/ui.js
+++ jss/ui.js
@@ -1,4 +1,19 @@
 ﻿
-document.getElementById("daysDisplay").textContent = formatAge(gameData.days)
-document.getElementById("yearsDisplay").textContent = formatAge(gameData.years)
-document.getElementById("moneyDisplay").textContent = formatAge(gameData.money)
+function updateUI() {
+    /*
+        NOTE: To ensure that performance does not decrease,
+        please only call the render function when the user can actually see the content.
+        If they can always see the content put the function call at the top of this function.
+
+        NOTE2: Do NOT render anything to the screen outside of this function.
+    */
+
+    // Always render the sidebar.
+    renderSideBar()
+}
+
+function renderSideBar() {
+    document.getElementById("daysDisplay").textContent = gameData.days
+    document.getElementById("yearsDisplay").textContent = gameData.years
+    document.getElementById("moneyDisplay").textContent = gameData.money
+}
