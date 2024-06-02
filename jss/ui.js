8567262520c091db78df0d0c2d01feac57d25a10
Dysoch <Dysoch@users.noreply.github.com>
2024-06-02 10:36:37 +0200

    adding display

---
+++ jss/ui.js
@@ -0,0 +1,4 @@
+﻿
+document.getElementById("daysDisplay").textContent = formatAge(gameData.days)
+document.getElementById("yearsDisplay").textContent = formatAge(gameData.years)
+document.getElementById("moneyDisplay").textContent = formatAge(gameData.money)
