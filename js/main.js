344eaf1ae16ce5b206c7ad8fac7757c2a68042cd
Dysoch <Dysoch@users.noreply.github.com>
2024-06-02 13:08:55 +0200

    small update

--- js/main.js
+++ js/main.js
@@ -1,31 +1,5 @@
-﻿
-function money() {
-    gameData.money += 1;
-
-    updateUI()
-    Unlocker()
-}
-
-function updateGeneratorsMoney() {
-    gameData.moneyPD = 0
-    if (gameData.unlocked.lemonade = 1, gameData.generators.money.lemonade >= 1) { gameData.moneyPD = (0.15 * gameData.generators.money.lemonade) }
-
-    gameData.moneyPY = gameData.moneyPD * 365
-}
-
-function Unlocker() {
+﻿function Unlocker() {
     if (gameData.money > 100, gameData.unlocked.lemonade = 0) { gameData.unlocked.lemonade = 1 }
-}
-
-function updateGenerators() {
-    if (gameData.unlocked.lemonade = 1, gameData.generators.money.lemonade >= 1) { gameData.money += 0.15 * gameData.generators.money.lemonade }
-}
-
-function moneyBuyGen(name) {
-    if (name = 'lemonade', gameData.unlocked.lemonade = 1, gameData.money > 100) { gameData.generators.money.lemonade += 1, gameData.money -= 100 }
-    
-    updateGeneratorsMoneyPS()
-    updateUI()
 }
 
 setInterval(function(){
---
+++ js/money.js
@@ -0,0 +1,25 @@
+﻿
+function money() {
+    gameData.money += 1;
+
+    updateUI()
+    Unlocker()
+}
+
+function updateGeneratorsMoney() {
+    gameData.moneyPD = 0
+    if (gameData.unlocked.lemonade = 1, gameData.generators.money.lemonade >= 1) { gameData.moneyPD = (0.15 * gameData.generators.money.lemonade) }
+
+    gameData.moneyPY = gameData.moneyPD * 365
+}
+
+function updateGenerators() {
+    if (gameData.unlocked.lemonade = 1, gameData.generators.money.lemonade >= 1) { gameData.money += 0.15 * gameData.generators.money.lemonade }
+}
+
+function moneyBuyGen(name) {
+    if (name = 'lemonade', gameData.unlocked.lemonade = 1, gameData.money > 100) { gameData.generators.money.lemonade += 1, gameData.money -= 100 }
+    
+    updateGeneratorsMoney()
+    updateUI()
+}
