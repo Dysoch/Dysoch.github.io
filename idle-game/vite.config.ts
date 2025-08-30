fe5e19107d8f44f595bbeb192585c3623dfae16e
Dysoch <frits@devreugd.nl>
2025-08-30 21:13:33 +0200

    Update vite.config.ts

--- idle-game/vite.config.ts
+++ idle-game/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Dysoch.github.io/", // 👈 important! repo name
+  base: "/idle-game/", // 👈 important! repo name
 });
